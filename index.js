a7995fb347361ff318ca3048827c9783697ed85e
Rollie Ma <rollie.ma@gmail.com>
2015-01-26 23:59:27 -0800

    Changed checkEvent model to ping model

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 
-var CheckEvent = require('../../models/checkEvent');
+// https://app.cyfe.com/api/push/54bc84094c37d986857583941523
+// {
+//     "data":  [
+//         {
+//             "Date":  "20130320",
+//             "Users":  "1"
+//         }
+//     ],
+//     "onduplicate":  {
+//         "Users":  "replace"
+//     },
+//     "color":  {
+//         "Users":  "#52ff7f"
+//     },
+//     "type":  {
+//         "Users":  "line"
+//     }
+// }
+
+// var CheckEvent = require('../../models/checkEvent');
+var Ping = require('../../models/ping');
 
 exports.initWebApp = function(options) {
     var config = options.config.slack;
 
-    CheckEvent.on('afterInsert', function(checkEvent) {
+    // CheckEvent.on('afterInsert', function(checkEvent) {
+
+    // });
+
+    Ping.on('afterInsert', function(ping) {
 
     });
 }
