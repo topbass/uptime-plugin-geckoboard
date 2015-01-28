8797ce1350e414f02f0d2ea8c8485e0327e80b1d
Rollie Ma <rollie.ma@gmail.com>
2015-01-28 01:36:48 -0800

    Alpha 1 version done

--- index.js
+++ index.js
@@ -18,8 +18,15 @@
 //     }
 // }
 
+//
+// cyfe:
+//   Google.com
+//     endpoint: [Cyfe Push API endpoint]
+//
+
 var http = require('http');
 var https = require('https');
+var moment = require('moment');
 var Ping = require('../../models/ping');
 
 exports.initWebApp = function(options) {
@@ -27,11 +34,15 @@ exports.initWebApp = function(options) {
 
     Ping.on('afterInsert', function(ping) {
         ping.findCheck(function(err, check) {
+            if (err) {
+                return console.error(err);
+            }
+
             if (!config[check.name]) {
                 return;
             }
 
-            var endpoint = config[check.name];
+            var endpoint = config[check.name].endpoint;
             var matches = endpoint.match(/(http|https):\/\/([^\/]+)(\/.*)?/);
 
             if (matches === null) {
@@ -43,6 +54,39 @@ exports.initWebApp = function(options) {
                 path: matches[3] || '/',
                 method: 'POST'
             };
+            var postdata = {
+                data: [
+                    {
+                        'Date': moment(check.qos.timestamp).format('YYYYMMDD'),
+                        'Response Time': check.qos.responseTime
+                    }
+                ],
+                onduplicate: {
+                    'Response Time': 'replace'
+                },
+                type: {
+                    'Response Time': 'line'
+                }
+            }
+
+            var req = (
+                matches[1] == 'https' ? https.request : http.request
+            )(httpOpts, function(res) {
+                if (res.statusCode == 200) {
+                    res.setEncoding('utf8');
+                    res.on('data', function (chunk) {
+                        console.log('Cyfe plugin response data [' + check.name + ']: ' + chunk);
+                    });
+                } else {
+                    console.error('Cyfe plugin response code: ' + res.statusCode);
+                    console.error('Cyfe plugin response headers: ' + JSON.stringify(res.headers));
+                }
+            });
+            req.on('error', function(_err) {
+                console.error('Cyfe plugin response error: ' + _err.message);
+            });
+            req.write(JSON.stringify(postdata));
+            req.end();
         });
     });
 }
