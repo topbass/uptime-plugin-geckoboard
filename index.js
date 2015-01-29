a6a5e0a3d9a7a6db4be32cdc6c1fbceb6ea1f90a
Rollie Ma <rollie.ma@gmail.com>
2015-01-29 01:49:35 -0800

    Uptime plugin for Geckoboard alpha v2

--- index.js
+++ index.js
@@ -1,92 +1,76 @@
 
-// https://app.cyfe.com/api/push/54bc84094c37d986857583941523
-// {
-//     "data":  [
-//         {
-//             "Date":  "20130320",
-//             "Users":  "1"
-//         }
-//     ],
-//     "onduplicate":  {
-//         "Users":  "replace"
-//     },
-//     "color":  {
-//         "Users":  "#52ff7f"
-//     },
-//     "type":  {
-//         "Users":  "line"
-//     }
-// }
-
 //
-// cyfe:
+// geckoboard:
 //   Google.com
-//     endpoint: [Cyfe Push API endpoint]
+//     - url: [push url]
+//       key: [widget key]
+//       widget: [widget type, possible values are monitoring, linechart and highcharts]
 //
 
+var fs = require('fs');
+var ejs = require('ejs');
 var http = require('http');
 var https = require('https');
 var moment = require('moment');
 var Ping = require('../../models/ping');
 
 exports.initWebApp = function(options) {
-    var config = options.config.cyfe;
+    var config = options.config.geckoboard;
+    var templateDir = __dirname + '/views/';
 
     Ping.on('afterInsert', function(ping) {
         ping.findCheck(function(err, check) {
             if (err) {
                 return console.error(err);
             }
-
             if (!config[check.name]) {
                 return;
             }
 
-            var endpoint = config[check.name].endpoint;
-            var matches = endpoint.match(/(http|https):\/\/([^\/]+)(\/.*)?/);
+            config.check[check.name].forEach(function(widget, index) {
+                var matches = widget.url.match(/(http|https):\/\/([^\/]+)(\/.*)?/);
 
-            if (matches === null) {
-                return;
-            }
+                if (matches === null) {
+                    return;
+                }
 
-            var httpOpts = {
-                host: matches[2],
-                path: matches[3] || '/',
-                method: 'POST'
-            };
-            var postdata = {
-                data: [
-                    {
-                        'Date': moment(check.qos.timestamp).format('YYYYMMDD'),
-                        'Response Time': check.qos.responseTime
+                var httpOpts = {
+                    host: matches[2],
+                    path: matches[3] || '/',
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
                     }
-                ],
-                onduplicate: {
-                    'Response Time': 'replace'
-                },
-                type: {
-                    'Response Time': 'line'
-                }
-            }
+                };
+                var filename = templateDir + widget.widget + '.ejs';
+                var renderOptions = {
+                    apiKey: config.apiKey,
+                    widget: widget,
+                    check: check,
+                    ping: ping,
+                    moment: moment
+                };
+                var json = ejs.render(fs.readFileSync(filename, 'utf8'), renderOptions);
 
-            var req = (
-                matches[1] == 'https' ? https.request : http.request
-            )(httpOpts, function(res) {
-                if (res.statusCode == 200) {
-                    res.setEncoding('utf8');
-                    res.on('data', function (chunk) {
-                        console.log('Cyfe plugin response data [' + check.name + ']: ' + chunk);
-                    });
-                } else {
-                    console.error('Cyfe plugin response code: ' + res.statusCode);
-                    console.error('Cyfe plugin response headers: ' + JSON.stringify(res.headers));
-                }
+                var req = (
+                    matches[1] == 'https' ? https.request : http.request
+                )(httpOpts, function(res) {
+                    if (res.statusCode == 200) {
+                        res.setEncoding('utf8');
+                        res.on('data', function (chunk) {
+                            console.log('Geckoboard plugin response data [' + check.name + ']: ' + chunk);
+                        });
+                    } else {
+                        console.error('Geckoboard plugin response code: ' + res.statusCode);
+                        console.error('Geckoboard plugin response headers: ' + JSON.stringify(res.headers));
+                    }
+                });
+                req.on('error', function(_err) {
+                    console.error('Geckoboard plugin response error: ' + _err.message);
+                });
+                req.write(json);
+                req.end();
             });
-            req.on('error', function(_err) {
-                console.error('Cyfe plugin response error: ' + _err.message);
-            });
-            req.write(JSON.stringify(postdata));
-            req.end();
         });
     });
 }
