ae88538f2aab8f2f223e8f74bbafc6ed06feacf1
Rollie Ma <rollie.ma@gmail.com>
2015-01-30 23:30:46 -0800

    @alpha v4, bug fix and line chart support

--- index.js
+++ index.js
@@ -25,55 +25,80 @@ exports.initWebApp = function(options) {
             if (err) {
                 return console.error(err);
             }
-            if (!config.check[check.name]) {
-                return;
-            }
 
-            config.check[check.name].forEach(function(widget, index) {
-                var matches = widget.url.match(/(http|https):\/\/([^\/]+)(\/.*)?/);
+            var period = 'day';
+            var begin = moment().startOf(period);
+            var end = moment().endOf(period);
 
-                if (matches === null) {
+            check.getStatsForPeriod(period, begin, end, function(err, stats) {
+                if (err) {
+                    return console.error(err);
+                }
+
+                var confCheck = [];
+
+                if (config.check[check.name]) {
+                    confCheck = config.check[check.name];
+                } else if (config.check[check._id]) {
+                    confCheck = config.check[check._id];
+                } else {
                     return;
                 }
 
-                var filename = templateDir + widget.widget + '.ejs';
-                var renderOptions = {
-                    apiKey: config.apiKey,
-                    widget: widget,
-                    check: check,
-                    ping: ping,
-                    moment: moment
-                };
-                var json = ejs.render(fs.readFileSync(filename, 'utf8'), renderOptions);
-                var httpOpts = {
-                    host: matches[2],
-                    path: matches[3] || '/',
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'Content-Length': json.length
-                    }
-                };
+                confCheck.forEach(function(widget, index) {
+                    var matches = widget.url.match(/(http|https):\/\/([^\/]+)(\/.*)?/);
 
-                var req = (
-                    matches[1] == 'https' ? https.request : http.request
-                )(httpOpts, function(res) {
-                    if (res.statusCode == 200) {
-                        res.setEncoding('utf8');
-                        res.on('data', function (chunk) {
-                            console.log('Geckoboard plugin response data [' + check.name + ']: ' + chunk);
-                        });
-                    } else {
-                        console.error('Geckoboard plugin response code: ' + res.statusCode);
-                        console.error('Geckoboard plugin response headers: ' + JSON.stringify(res.headers));
+                    if (matches === null) {
+                        return;
                     }
+
+                    var filename = templateDir + widget.widget + '.ejs';
+                    var renderOptions = {
+                        apiKey: config.apiKey,
+                        widget: widget,
+                        check: check,
+                        ping: ping,
+                        stats: stats,
+                        moment: moment
+                    };
+                    var postData = ejs.render(fs.readFileSync(filename, 'utf8'), renderOptions);
+                    var httpOptions = {
+                        host: matches[2],
+                        path: matches[3] || '/',
+                        method: 'POST',
+                        headers: {
+                          'Content-Type': 'application/json',
+                          'Content-Length': postData.length
+                        }
+                    };
+                    var protocol = matches[1];
+
+                    sendHttpRequest(check.name, protocol, httpOptions, postData);
                 });
-                req.on('error', function(_err) {
-                    console.error('Geckoboard plugin response error: ' + _err.message);
-                });
-                req.write(json);
-                req.end();
             });
         });
     });
+}
+
+function sendHttpRequest(name, protocol, options, data) {
+    var req = (
+        protocol == 'https' ? https.request : http.request
+    )(options, function(res) {
+        if (res.statusCode == 200) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Geckoboard plugin response data [' + name + ']: ' + chunk);
+            });
+        } else {
+            console.error('Geckoboard plugin response code: ' + res.statusCode);
+            console.error('Geckoboard plugin response headers: ' + JSON.stringify(res.headers));
+        }
+    });
+
+    req.on('error', function(_err) {
+        console.error('Geckoboard plugin response error: ' + _err.message);
+    });
+
+    req.write(data);
+    req.end();
 }
